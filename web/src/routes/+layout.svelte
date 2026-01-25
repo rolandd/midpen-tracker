e0f94dbcb16578fed37e734fe9fc05df9e4403cb
Roland Dreier <roland@kernel.org>
2026-01-25 12:10:44 -0800

    Add opengraph / twitter tags for social posts

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -10,10 +10,10 @@
 </script>
 
 <svelte:head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Midpen Tracker</title>
+
 	<meta name="description" content="Track your Strava activities in Midpen Open Space Preserves" />
+
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
